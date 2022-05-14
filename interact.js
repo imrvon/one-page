e9dfc4b67d6c01026e787dc71059d6d352b0fa90
VoN <webdesignervon@gmail.com>
2022-05-14 22:57:26 +0100

    Fixed Mobile Menu Not Going To Section When Clicked

--- interact.js
+++ interact.js
@@ -1,4 +1,5 @@
 const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const mobile = document.querySelector('.header .nav-bar .nav-list ul');
 const header = document.querySelector('.header.container')
 
@@ -9,9 +10,16 @@ hamburger.addEventListener('click',()=>{
 
 document.addEventListener('scroll',()=>{
     var scroll_position = window.scrollY;
-    if(scroll_position > 50) {
+    if(scroll_position > 150) {
         header.style.backgroundColor = '#29323c';
     } else {
         header.style.backgroundColor = 'transparent';
     }
-})
+});
+
+menu_item.forEach((item) => {
+    item.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        mobile.classList.toggle('active');
+    });
+});
